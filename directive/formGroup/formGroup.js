f893faa93e0a13c64a9158fe5853e6d14d4c269d
swaylq <501527961@qq.com>
2015-05-14 18:08:59 +0800

    Add the webUploader

--- directive/formGroup/formGroup.js
+++ directive/formGroup/formGroup.js
@@ -14,7 +14,7 @@ MyApp.directive('formGroup', function(){
         // controller: function($scope, $element, $attrs, $transclude) {},
         // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
         restrict: 'EA', // E = Element, A = Attribute, C = Class, M = Comment
-        templateUrl: 'directive/FormGroup.html',
+        templateUrl: 'directive/formGroup/formGroup.html',
         replace: true,
         transclude: true,
         // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
